346ce6f39f9f7a3fe055e719915c66a765aa8c84
applicatin.yml <1013398588@qq.com>
2018-01-16 15:36:01 +0800

    http header fetch app_key and app_secret

--- src/test/java/com/bullyun/param/user/UserAuthcodeParamTest.java
+++ src/test/java/com/bullyun/param/user/UserAuthcodeParamTest.java
@@ -8,13 +8,11 @@ public class UserAuthcodeParamTest {
     @Test
     public void userAuthcodeTest() {
         DefaultSentClient defaultSentClient = new DefaultSentClient(
-                "8Wa227sQ00S33p4y",
+                "8Wa227sQ00S33p4y1",
                 "RlA8aCPlsuATT227kKTg003ncP35HYRI",
                 "http://www.vssweb.net:10210");
         UserAuthcodeParam param = new UserAuthcodeParam();
         param.setPhone("18458306299");
-        param.setCaptcha("fljy");
-        param.setSession("58075be0-1e6e-4a91-bc1f-61a445dfe9f3");
         param.setCountryCode("86");
         param.setLocale("zh_CN");
         param.setValid("sms");
